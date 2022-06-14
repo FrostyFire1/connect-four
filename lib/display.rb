be16bcd86e00221ed179996b4e61ccf6456a96f6
FrostyFire1 <KamilBaron02@gmail.com>
2022-06-14 20:08:53 +0200

    Add show_state

--- lib/display.rb
+++ lib/display.rb
@@ -1,5 +1,17 @@
 class Display
   def initialize
-    @state = Array.new(6) {Array.new(7, ⬤)}
+    @state = Array.new(6) {Array.new(7, '⬤')}
+  end
+
+  def show_state
+    vertical_line = ' │'
+    horizontal_line = '─'
+    @state.each do |row|
+      row_state = row.join(vertical_line)
+      puts "#{vertical_line}#{row_state}#{vertical_line}"
+    end
+    rows = @state.length
+    bottom_line = horizontal_line * (rows * 3 + 2)
+    puts " └#{bottom_line}┘"
   end
 end
