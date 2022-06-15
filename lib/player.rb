c4ff65302b7a1271e884ef54114fc179bef0f5fc
FrostyFire1 <KamilBaron02@gmail.com>
2022-06-15 09:41:28 +0200

    Add to_ansii()

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,20 @@
 class Player
   def initialize(name, color)
     @name = name
-    @color = color
+    @color = to_ansii(color)
+  end
+
+  def to_ansii(color)
+    foreground = "\e[38;5;"
+    case color
+    when "red"
+      foreground << "9m"
+    when "blue"
+      foreground << "12m"
+    when "yellow"
+      foreground << "11m"
+    when "green"
+      foreground << "10m"
+    end
   end
 end
---
+++ spec/player_spec.rb
@@ -0,0 +1,30 @@
+require "player.rb"
+
+describe Player do
+
+  describe "#to_ansii" do
+    subject(:ansii_encoder) { described_class.new("John", "none")}
+    context "when supported ansii color is chosen" do
+
+      it "returns correct ansii code for red" do
+        ansii_color = ansii_encoder.to_ansii("red")
+        expect(ansii_color).to eq("\e[38;5;9m")
+      end
+
+      it "returns correct ansii code for blue" do
+        ansii_color = ansii_encoder.to_ansii("blue")
+        expect(ansii_color).to eq("\e[38;5;12m")
+      end
+
+      it "returns correct ansii code for yellow" do
+        ansii_color = ansii_encoder.to_ansii("yellow")
+        expect(ansii_color).to eq("\e[38;5;11m")
+      end
+
+      it "returns correct ansii code for green" do
+        ansii_color = ansii_encoder.to_ansii("green")
+        expect(ansii_color).to eq("\e[38;5;10m")
+      end
+    end
+  end
+end
