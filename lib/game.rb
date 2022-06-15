d04570d24bba8c8c5ef9fdf8a622c58b346f860e
FrostyFire1 <KamilBaron02@gmail.com>
2022-06-15 18:17:59 +0200

    Add player name input

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,13 @@
 require_relative "connect_four.rb"
 require_relative "player.rb"
-player1 = Player.new("John", "red")
-player2 = Player.new("Mark","yellow")
+
+puts "Welcome to connect four!"
+puts "Player #1, please enter your name"
+player1_name = gets.chomp
+puts "Player #2, please enter your name"
+player2_name = gets.chomp
+player1 = Player.new(player1_name, "red")
+player2 = Player.new(player2_name,"yellow")
 
 game = ConnectFour.new(player1, player2)
 
